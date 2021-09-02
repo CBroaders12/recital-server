f8a9470b6915df322790efcd1ca1963d2b6d66c0
CBroaders <67294112+CBroaders12@users.noreply.github.com>
2021-09-02 11:05:38 -0400

    case insensitive song queries

--- controllers/open/songController.js
+++ controllers/open/songController.js
@@ -2,14 +2,55 @@ const models = require('../../models');
 
 const { Router } = require('express');
 const { NotFoundError } = require('../../errors');
+const { sequelize } = require('../../models');
 
 const songController = Router();
 
 songController.get('/', async (req, res, next) => {
 	try {
-		const { offset = 0, limit = 20 } = req.query;
+		let {
+			offset = 0,
+			limit = 20,
+			language,
+			composer,
+			author,
+			title,
+		} = req.query;
+
+		// TODO: Refactor this for conciseness
+		// Make search case insensitive
+		const where = {};
+		if (language) {
+			where.language = sequelize.where(
+				sequelize.fn('LOWER', sequelize.col('language')),
+				'LIKE',
+				`%${language.toLowerCase()}%`
+			);
+		}
+		if (composer) {
+			where.composer = sequelize.where(
+				sequelize.fn('LOWER', sequelize.col('composer')),
+				'LIKE',
+				`%${composer.toLowerCase()}%`
+			);
+		}
+		if (author) {
+			where.author = sequelize.where(
+				sequelize.fn('LOWER', sequelize.col('author')),
+				'LIKE',
+				`%${author.toLowerCase()}%`
+			);
+		}
+		if (title) {
+			where.title = sequelize.where(
+				sequelize.fn('LOWER', sequelize.col('title')),
+				'LIKE',
+				`%${title.toLowerCase()}%`
+			);
+		}
 
 		const songs = await models.song.findAll({
+			where,
 			offset,
 			limit,
 		});
