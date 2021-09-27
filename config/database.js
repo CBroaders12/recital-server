2dd71b760c70d270689cbaeb3cab77156ec54f93
CBroaders <67294112+CBroaders12@users.noreply.github.com>
2021-09-27 12:43:53 -0400

    sequelizerc file

---
+++ config/database.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DEV_DB,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.TEST_DB,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    logging: false,
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.PROD_DB,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
